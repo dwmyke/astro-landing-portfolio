43ffcb68389661009ec9d4f750f4411f5459e814
Mikel-Jon West <dwmyke@gmail.com>
2022-10-05 22:48:57 -0700

    Updated meta Head

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://mikewest.site/",
   integrations: [tailwind()],
   vite: {
     ssr: {
-      external: ["@11ty/eleventy-img", "svgo"],
-    },
-  },
+      external: ["@11ty/eleventy-img", "svgo"]
+    }
+  }
 });
